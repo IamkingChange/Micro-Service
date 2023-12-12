0e1c4f5e628da7516360b3e435751c3f9ee353ff
IamkingChange <85507839+IamkingChange@users.noreply.github.com>
2023-12-12 17:58:08 +0800

    Create NameProperties.java

---
+++ src/NameApp-src/main/java/org.example/NameProperties.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class NameProperties {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
